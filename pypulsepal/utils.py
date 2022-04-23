30a9c93d95f4e960307bdc3e3315039e8a7fc63b
Lars B. Rollik <l.b.rollik@protonmail.com>
2022-04-23 21:46:20 +0100

    updated build system to toml/cfg standard and updated misc. doc files. (#3)

--- pypulsepal/utils.py
+++ pypulsepal/utils.py
@@ -2,10 +2,14 @@ import numpy as np
 
 
 def volts_to_bytes(volt=None, dac_bitMax=None):
+    """Convert volts to bytes, given scaling by DAC maximum bit value"""
     return np.ceil(((volt + 10) / float(20)) * dac_bitMax)
 
 
 def encode_message(*message_parts, encoding=None):
-    message = [np.array(part, dtype=encoding).tobytes() for part in message_parts]
+    """Encode message as byte string for serial interface communication"""
+    message = [
+        np.array(part, dtype=encoding).tobytes() for part in message_parts
+    ]
     out = b"".join(message)
     return out
